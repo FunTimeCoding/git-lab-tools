ceb6642a9e696c789a16dfd454cb845f3a412192
Alexander Reitzel <funtimecoding@gmail.com>
2016-12-01 02:45:16 +0100

    add toggle to list repos script to show full path

--- bin/list-repos.sh
+++ bin/list-repos.sh
@@ -2,8 +2,20 @@
 
 DIRECTORY=$(dirname "${0}")
 SCRIPT_DIRECTORY=$(cd "${DIRECTORY}"; pwd)
+
+usage()
+{
+    echo "Usage: ${0} [--with-vendor]"
+}
+
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}/../lib/gitlab.sh"
 JSON=$(${REQUEST} "${API_URL}/projects/owned")
-NAMES=$(echo "${JSON}" | jsawk -n "out(this.name)")
+
+if [ "${1}" = --with-vendor ]; then
+    NAMES=$(echo "${JSON}" | jsawk -n "out(this.path_with_namespace)")
+else
+    NAMES=$(echo "${JSON}" | jsawk -n "out(this.name)")
+fi
+
 echo "${NAMES}"
