3770fbd69c23024cc3a175fd606855703b64b552
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-06 17:48:50 +0200

    add optional namespace argument to get project identifier

--- bin/get-project-identifier.sh
+++ bin/get-project-identifier.sh
@@ -5,13 +5,14 @@ SCRIPT_DIRECTORY=$(cd "${DIRECTORY}"; pwd)
 
 usage()
 {
-    echo "Usage: ${0} NAME"
+    echo "Usage: ${0} NAME [NAMESPACE]"
 }
 
 # shellcheck source=/dev/null
 . "${SCRIPT_DIRECTORY}/../lib/git_lab_tools.sh"
 
 NAME="${1}"
+NAMESPACE="${2}"
 
 if [ "${NAME}" = "" ]; then
     usage
@@ -19,4 +20,20 @@ if [ "${NAME}" = "" ]; then
     exit 1
 fi
 
-${REQUEST} "${INTERFACE_LOCATOR}/projects?search=${NAME}" | jsawk -n "if (this.name == '${NAME}') out(this.id)"
+RESPONSE=$(${REQUEST} "${INTERFACE_LOCATOR}/projects?search=${NAME}")
+
+if [ "${NAMESPACE}" = "" ]; then
+    IDENTIFIERS=$(echo "${RESPONSE}" | jsawk -n "if (this.name == '${NAME}') out(this.id)")
+else
+    IDENTIFIERS=$(echo "${RESPONSE}" | jsawk -n "if (this.name == '${NAME}' && this.path_with_namespace == '${NAMESPACE}') out(this.id)")
+fi
+
+COUNT=$(echo "${IDENTIFIERS}" | wc -l | xargs)
+
+if [ "${COUNT}" = 1 ]; then
+    echo "${IDENTIFIERS}"
+else
+    echo "More than one project returned."
+
+    exit 1
+fi
