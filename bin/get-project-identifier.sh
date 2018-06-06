2ff8b52c2c6337cd4558bdf52841a8578192d2d3
Alexander Reitzel <funtimecoding@gmail.com>
2018-06-06 17:54:32 +0200

    cover empty return case

--- bin/get-project-identifier.sh
+++ bin/get-project-identifier.sh
@@ -25,12 +25,16 @@ RESPONSE=$(${REQUEST} "${INTERFACE_LOCATOR}/projects?search=${NAME}")
 if [ "${NAMESPACE}" = "" ]; then
     IDENTIFIERS=$(echo "${RESPONSE}" | jsawk -n "if (this.name == '${NAME}') out(this.id)")
 else
-    IDENTIFIERS=$(echo "${RESPONSE}" | jsawk -n "if (this.name == '${NAME}' && this.path_with_namespace == '${NAMESPACE}') out(this.id)")
+    IDENTIFIERS=$(echo "${RESPONSE}" | jsawk -n "if (this.name == '${NAME}') && (this.path_with_namespace == '${NAMESPACE}') out(this.id)")
 fi
 
 COUNT=$(echo "${IDENTIFIERS}" | wc -l | xargs)
 
-if [ "${COUNT}" = 1 ]; then
+if [ "${COUNT}" = "" ]; then
+    echo "Empty search result."
+
+    exit 1
+elif [ "${COUNT}" = 1 ]; then
     echo "${IDENTIFIERS}"
 else
     echo "More than one project returned."
